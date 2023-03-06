bff0f82b1f781fc9f58f3304b872cd5382cd3a20
Abel <UNO@DESKTOP-PIL3JOM>
2023-03-06 13:32:21 +0100

    // Nombre y valor de la nota

---
+++ src/main/java/com/mycompany/abelgit/AbelGIT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abelgit;
+
+/**
+ *
+ * @author Abel
+ */
+public class AbelGIT {
+
+    public static void main(String[] args) {
+        
+       // Abel Santaella Sarrias
+        int nota = 8;
+        //valor de la nota e imprimimos el mensaje
+        System.out.println("Abel Santaella Sarrias " + nota );
+    }
+}
